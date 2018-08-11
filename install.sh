b66f48f1652fb34c716887c11c7618de995a3e2f
dataphobe <dataphobe@datapho.be>
2018-08-11 14:53:24 +0200

    some setup scripts

---
+++ install.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+# install hadoop echo system
+# hadoop https://www.digitalocean.com/community/tutorials/how-to-install-hadoop-in-stand-alone-mode-on-ubuntu-16-04
+# cassandra + kafka + spark just downlaod them and put them in the home/username directory
+# install anaconda notebook
+# install tensorflow
+
+# Install powerline fonts for termite and shit
+# clone the termite intstallation from your github
+
+# TODO: adde to references http://www.howardism.org/Technical/Emacs/new-window-manager.html
+
+sudo apt-get install -y xinit
+sudo apt-get install -y emacs
+sudo apt-get install -y compton
+sudo apt-get install -y firefox thunderbird cifs-utils
+sudo apt-get install -y imagemagick nodejs
+sudo apt-get install -y openjdk-8-jdk
+sudo apt-get install -y emacs
+sudo apt-get install -y g++ libgtk-3-dev gtk-doc-tools gnutls-bin valac intltool libpcre2-dev libglib3.0-cil-dev libgnutls28-dev libgirepository1.0-dev libxml2-utils gperf build-essential
+sudo apt-get install -y libpng-dev zlib1g-dev
+sudo apt-get install -y libpoppler-glib-dev
+sudo apt-get install -y libpoppler-private-dev
+sudo apt-get install -y okular
+sudo apt-get install -y dunst
+sudo apt-get install -y ranger
+sudo apt-get install -y qutebrowser
+sudo apt-get install -y maven
+
+# make sure you first have python env setup
+# Either of these
+pip install rope
+pip install jedi
+# flake8 for code checks
+pip install flake8
+# and autopep8 for automatic PEP8 formatting
+pip install autopep8
+# and yapf for code formatting
+pip install yapf
+pip install opencv-python
